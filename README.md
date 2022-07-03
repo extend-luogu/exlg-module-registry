efaf0fe4682706eb46f7cd99afe9a86917f9de62
汪心禾 <wangxinhe06@gmail.com>
2022-07-03 12:18:21 +0800

    Initialization

---
+++ README.md
@@ -0,0 +1,5 @@
+# exlg-module-registry
+
+The Official Module Registry of Extend Luogu
+
+EXLG 模块官方源
---
+++ index.ts
@@ -0,0 +1,12 @@
+import * as fs from "node:fs/promises";
+import glob from "glob";
+import YAML from "yaml";
+
+Promise.all(
+  glob
+    .sync("modules/**/*.{yml,yaml}")
+    .map(async (file) => YAML.parse(await fs.readFile(file, "utf-8")))
+).then(async (mods) => {
+  await fs.mkdir("dist", { recursive: true });
+  await fs.writeFile("dist/index.json", JSON.stringify(mods));
+});
